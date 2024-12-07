9838abc86b637251b737480d69328e29f9576005
naidubonigi <naidubonigi2005@gmail.com>
2024-12-07 12:52:49 +0530

    first commit

---
+++ src/main/java/com/klef/jfsd/exam/ClientDemo.java
@@ -0,0 +1,43 @@
+package com.klef.jfsd.exam;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+public class ClientDemo {
+    public static void main(String[] args) {
+        // Create a SessionFactory
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                                                     .addAnnotatedClass(Department.class)
+                                                     .buildSessionFactory();
+
+        // Get the current session by explicitly opening a session
+        Session session = factory.openSession();
+
+        try {
+            // Start a transaction
+            session.beginTransaction();
+
+            // Update the department with id = 1 (make sure this id exists)
+            String hql = "update Department set name = ?1, location = ?2 where id = ?3"; // Use 'id' in the query
+            Query query = session.createQuery(hql);
+            query.setParameter(1, "Updated Department Name");
+            query.setParameter(2, "New Location");
+            query.setParameter(3, 1); // Assuming id = 1 exists in the database
+
+            // Execute the update
+            int result = query.executeUpdate();
+            System.out.println("Number of rows affected: " + result);
+
+            // Commit the transaction
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close the session
+            session.close();
+            factory.close();
+        }
+    }
+}
